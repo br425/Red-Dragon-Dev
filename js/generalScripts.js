80bea0a0676343a85a35b670961ff74276ef6292
br425 <br425@drexel.edu>
2018-03-05 16:02:20 -0500

    beta v4.0.1

--- js/generalScripts.js
+++ js/generalScripts.js
@@ -1,37 +1,63 @@
 
 
 
-$(".textBox a").hover(function(){
-    $('.redTextBox').addClass('boxHover');
-    $(".redTextBox a img").attr("src","icons/rightArrow.svg");    
-    }, function(){
-    $('.textBox').removeClass('boxHover');
-    $(".redTextBox a img").attr("src","icons/rightArrowIn.svg");    
-});
+                function onReady(callback) {
+                    var intervalID = window.setInterval(checkReady, 1000);
 
-$("#voyageDot").hover(function(){
-    $("#voyageDot img").attr("src","icons/rightArrowIn.svg");    
-    }, function(){
-    $("#voyageDot img").attr("src","icons/rightArrow.svg");    
-});
+                    function checkReady() {
+                        if (document.getElementsByTagName('body')[0] !== undefined) {
+                            window.clearInterval(intervalID);
+                            callback.call(this);
+                        }
+                    }
+                }
 
-$("#archiveDot").hover(function(){
-    $("#archiveDot img").attr("src","icons/rightArrowIn.svg");    
-    }, function(){
-    $("#archiveDot img").attr("src","icons/rightArrow.svg");    
-});
+                function show(id, value) {
+                    document.getElementById(id).style.opacity = value ? 1 : 0;
+                }
 
-$("#aboutDot").hover(function(){
-    $("#aboutDot img").attr("src","icons/rightArrowIn.svg");    
-    }, function(){
-    $("#aboutDot img").attr("src","icons/rightArrow.svg");    
-});
+                onReady(function () {
+                    show('content', true);
+                    show('loading', false);
+                });
+
+
+
+// $(".textBox a").hover(function(){
+//     $('.redTextBox').addClass('boxHover');
+//     $(".redTextBox a img").attr("src","icons/rightArrow.svg");    
+//     }, function(){
+//     $('.textBox').removeClass('boxHover');
+//     $(".redTextBox a img").attr("src","icons/rightArrowIn.svg");    
+// });
+
+// $("#voyageDot").hover(function(){
+//     $("#voyageDot img").attr("src","icons/rightArrowIn.svg");    
+//     }, function(){
+//     $("#voyageDot img").attr("src","icons/rightArrow.svg");    
+// });
+
+// $("#archiveDot").hover(function(){
+//     $("#archiveDot img").attr("src","icons/rightArrowIn.svg");    
+//     }, function(){
+//     $("#archiveDot img").attr("src","icons/rightArrow.svg");    
+// });
+
+// $("#aboutDot").hover(function(){
+//     $("#aboutDot img").attr("src","icons/rightArrowIn.svg");    
+//     }, function(){
+//     $("#aboutDot img").attr("src","icons/rightArrow.svg");    
+// // });
 
 
 $(".textBox a").hover(function(){
     $(".boxTop").attr("src","icons/boxTopHover.svg");
+    $(".textArea").attr("style","border-color: #f23535;");
+    $(".a").attr("style","background-color: #f23535;");
     }, function(){
     $(".boxTop").attr("src","icons/boxTop.svg");
+    $(".textArea").attr("style","border-color: #900000;");
+    $(".a").attr("style","background-color: #900000;");
 });
 
 $(".bigStopText a").hover(function(){
@@ -48,13 +74,13 @@ $(".bigStopText a").hover(function(){
 });
 
 
-$("#voyageNavA").hover(function(){
-    $("#voyageLogo").attr("src","icons/logo-hoayver.svg");
+$("#navA").hover(function(){
+    $("#voyageLogo").attr("src","icons/logo-hover.svg");
     }, function(){
     $("#voyageLogo").attr("src","icons/logo-white.svg");
 });
 
-// $("#voyageNavA").hover(function(){
+// $("#navA").hover(function(){
 //     $("#voyageTitle").attr("src","icons/title-logo-red.svg");
 //     }, function(){
 //     $("#voyageTitle").attr("src","icons/title-logo.svg");
@@ -65,17 +91,18 @@ $("#voyageTitle").hover(function(){
     $("#voyageTitle").attr("src","icons/logo-white.svg");
 });
 
-$("#voyageNavA").hover(function(){
-    $("#voyageNavA h1").css("color","#f23535");
+$("#navA").hover(function(){
+    $("#navA h1").css("color","#f23535");
     }, function(){
-    $("#voyageNavA h1").css("color","#fffcfc");
+    $("#navA h1").css("color","#fffcfc");
 });
 
 
 $("#compassButton").click(function(){
     console.log('clicked nav');
     $(".barContainer").toggleClass("openMenu");
-    $('#voyageNav').toggleClass('fullMenu');
+    $("#homeLinks").toggleClass("openMenu");
+    $('#nav').toggleClass('fullMenu');
     $("#compassButton").toggleClass("activeCompass");
 });
 
